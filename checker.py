5fe1e4bb69648850e8f82f656ed9a2400301fcbe
rodionov12 <rodionov12@gmail.com>
2017-02-18 01:50:25 +0300

    Modify checker

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
 import subprocess
+import threading
 import schedule
 import time
 import MySQLdb
@@ -21,7 +22,22 @@ HOSTNAME = "127.0.0.1"
 
 #start = time.time()
 
-conn = MySQLdb.connect(user="root", password="keklol", host="localhost", db="ctf")
+conn = MySQLdb.connect(user="root", password="keklol1488", host="localhost", db="ctf")
+
+def mysql_query(self, query, args=(), one=True, is_destructive=False):
+    assert(isinstance(query, str))
+    assert(isinstance(args, tuple))
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        if is_destructive:
+            db.commit()
+        if one:
+            return cursor.fetchone()
+        else:
+            return cursor.fetchall()
+    finally:
+        cursor.close()
 
 def check(hostname):
     code = 110
@@ -53,7 +69,9 @@ def put(hostname, flag):
         code = 104
         outs = "Host unreachable"
         popen.kill()
-    return outs, code
+    exit(0)
+    print("{}: {}".format(hostname, code))
+    #return outs, code
 
 def get(hostname, flag):
     code = 110
@@ -70,10 +88,39 @@ def get(hostname, flag):
         popen.kill()
     return outs, code
 
-schedule.every(0.133).minutes.do(check, HOSTNAME)
-schedule.every(2).minutes.do(put, HOSTNAME, "KEK")
+
+def putall():
+    hosts = []
+    for i in range(2, 254):
+        t = threading.Thread(target=put, args=["192.168.100." + str(i), "KEK"])
+        t.start()
+        hosts.append(t)
+    for i in hosts:
+        i.join()
+    return 0
+
+def checkall():
+    hosts = []
+    for i in range(2, 254):
+        t = threading.Thread(target=put, args=["192.168.100." + str(i), "KEK"])
+        t.start()
+        hosts.append(t)
+    for i in hosts:
+        i.join()
+    return 0
+
+# def getall():
+#     hosts=""
+#     print("KEK")
+#     return 0
+#
+# schedule.every(0.133).minutes.do(check, HOSTNAME)
+# schedule.every(3).minutes.do(put, HOSTNAME, "KEK")
+
+#schedule.every(0.133).minutes.do(check, HOSTNAME)
+schedule.every(0.1).minutes.do(putall)
 
 if __name__ == '__main__':
     while 1:
         schedule.run_pending()
-        time.sleep(0.1)
+        time.sleep(0.01)
